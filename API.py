6302109d484c7cb2fbd1aba7e95198973c52088f
Saipy <zimens05@mail.ru>
2023-02-18 17:50:49 +0300

    API arch change

--- API.py
+++ API.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from starlette.middleware.cors import CORSMiddleware
-from FastAPI.endpoints import users, services, complaints, apartments, discussions, voting, stories
-from Database.base import AsyncDatabase
+from FastAPI.endpoints import users, services, complaints, apartments, discussions, voting, stories, base
 
 app = FastAPI()
 
@@ -13,6 +12,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(base.router, tags=['Base'])
 app.include_router(users.router, tags=['Users'], prefix='/users')
 app.include_router(services.router, tags=['Services'], prefix='/services')
 app.include_router(complaints.router, tags=['Complaints'], prefix='/complaints')
@@ -20,18 +21,6 @@ app.include_router(apartments.router, tags=['Apartments'], prefix='/apartments')
 app.include_router(discussions.router, tags=['Discussions'], prefix='/discussions')
 app.include_router(voting.router, tags=['Voting'], prefix='/vote')
 app.include_router(stories.router, tags=['Stories'], prefix='/stories')
-
-
-@app.get('/', name='Root')
-async def api_root():
-    return "This is root API, documentation is available at /docs or /redoc"
-
-
-@app.get("/db", name="Create database")
-async def create_db():
-    await AsyncDatabase.create_db()
-    return "Database created"
-
 
 if __name__ == '__main__':
     uvicorn.run("API:app", host='127.0.0.1', reload=True)
---
+++ FastAPI/endpoints/base.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from Database.base import AsyncDatabase
+
+router = APIRouter()
+
+@router.get('/', name='Root')
+async def api_root():
+    return "This is root API, documentation is available at /docs or /redoc"
+
+
+@router.get("/db", name="Create database")
+async def create_db():
+    await AsyncDatabase.create_db()
+    return "Database created"
